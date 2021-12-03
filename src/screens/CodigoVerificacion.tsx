d892e59315f9a7cd21a86fa6d0c655da7f7ce57f
DarlinCondC <darlinapraez@condorcoin.co>
2021-12-03 17:22:55 -0500

    Pantallas recuperadas, codigo verificación, crear cuenta, importar cuenta e imagen mano

--- src/screens/CodigoVerificacion.tsx
+++ src/screens/CodigoVerificacion.tsx
@@ -1,38 +1,49 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View, TextInput, Image, Button, Alert } from 'react-native'
+import { Text, StyleSheet, View, TextInput, Image, Button, Alert, TouchableOpacity } from 'react-native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+
+import { generateMnemonic, mnemonicToSeed, createAccount, getBalance, getToken,sendTokenTransaction } from '../../api';
 
 const CodigoVerificacion = () => {
     
         return (
-        <View style={styles.body}>
+        <KeyboardAwareScrollView resetScrollToCoords={{ x: 0, y: 0 }}
+        contentContainerStyle={styles.body}
+        scrollEnabled={false}>
             <View style={styles.containeruno}>
-            <Image source={require('./img/mountain.png')} style={styles.logo} />
-            <Text style={styles.text1} numberOfLines={2}>CREAR CODIGO DE SEGURIDAD</Text>
-            <View style={styles.containerunorama}>
-                <TextInput style={styles.TextInput} maxLength={1} keyboardType="numeric"></TextInput>
-                <TextInput style={styles.TextInput} maxLength={1} keyboardType="numeric"></TextInput>
-                <TextInput style={styles.TextInput} maxLength={1} keyboardType="numeric"></TextInput>
-                <TextInput style={styles.TextInput} maxLength={1} keyboardType="numeric"></TextInput>
+                <Image source={require('./img/logocolor.png')} style={styles.logo} />
+            
+            <View style={styles.containerBlanco}>
+            
             </View>
-            </View>
-            <View style={styles.container2}>
-            <Text style={styles.text1} numberOfLines={2}>CONFIRMAR CODIGO DE SEGURIDAD</Text>
-            <View style={styles.containerunorama}>
-                <TextInput style={styles.TextInput} maxLength={1} keyboardType="numeric"></TextInput>
-                <TextInput style={styles.TextInput} maxLength={1} keyboardType="numeric"></TextInput>
-                <TextInput style={styles.TextInput} maxLength={1} keyboardType="numeric"></TextInput>
-                <TextInput style={styles.TextInput} maxLength={1} keyboardType="numeric"></TextInput>
+            <View style={styles.containerBlanco2}>
+                <Image source={require('./img/manito.png')} style={styles.logomano} />
+                <Text style={styles.textuno} numberOfLines={2}>Crear código de seguridad</Text>
+                <View style={styles.containerunorama}>  
+                    <TextInput style={styles.TextInput} maxLength={1} keyboardType="numeric"></TextInput>
+                    <TextInput style={styles.TextInput} maxLength={1} keyboardType="numeric"></TextInput>
+                    <TextInput style={styles.TextInput} maxLength={1} keyboardType="numeric"></TextInput>
+                    <TextInput style={styles.TextInput} maxLength={1} keyboardType="numeric"></TextInput>
+                </View>
+                    <Text style={styles.textdos} numberOfLines={2}>Confirmar código de seguridad</Text>
+                <View style={styles.containerunorama}>
+                    <TextInput style={styles.TextInput2} maxLength={1} keyboardType="numeric"></TextInput>
+                    <TextInput style={styles.TextInput2} maxLength={1} keyboardType="numeric"></TextInput>
+                    <TextInput style={styles.TextInput2} maxLength={1} keyboardType="numeric"></TextInput>
+                    <TextInput style={styles.TextInput2} maxLength={1} keyboardType="numeric"></TextInput>
+                </View>
             </View>
             <View style={styles.containerdos}>
-            <Button
-                title="CONFIRMAR"
-                color="#5B298A"
-                //AZUL: #26196B
-                onPress={() => Alert.alert('Codigo')}/>
+            <TouchableOpacity
+                style={styles.botonCont}
+                onPress={() => Alert.alert('Codigo...')}>
+                <Text style={styles.textoBoton}>CONFIRMAR</Text>
+            </TouchableOpacity>
+            
             </View>
             </View>
             
-        </View>
+        </KeyboardAwareScrollView>
         )
     }
 
@@ -44,8 +55,6 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '100%',
         flex: 1,
-        paddingLeft: '5%',
-        paddingRight: '5%',
         paddingTop: '8%'
     },
     containeruno: {
@@ -54,30 +63,66 @@ const styles = StyleSheet.create({
     },
     container2: {
         alignItems: 'center',
-        paddingTop: '20%'
+        paddingTop: '30%',
+        backgroundColor: 'white'
+    },
+    containerBlanco: {
+        width: '100%',
+        marginTop: 10,
+        alignItems: 'center',
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+        borderWidth: 5,
+        borderColor: '#b5b2b6',
+        borderStyle: 'solid',
+        paddingTop: '40%',
+    },
+    containerBlanco2: {
+        width: '100%',
+        marginTop: '-40%',
+        borderTopLeftRadius:28,
+        borderTopRightRadius: 28,
+        alignItems: 'center',
+        backgroundColor: 'white',
+        paddingLeft: '10%',
+        paddingRight: '10%',
+        paddingTop: '10%',
     },
     logo: {
-        marginTop: '25%',
+        marginTop: '0%',
         width: 150,
         height: 150,
         resizeMode: 'contain',
     },
-    text1: {
-        fontFamily: 'Roboto',
+    logomano: {
+        top: '0%',
+        alignItems: 'center',
+        width: 100,
+        height: 100,
+        
+    },
+    textuno: {
+        top: '10%',
+        fontSize: 15,
+        fontWeight: 'bold',
+        color: '#625d5b'
+    },
+    textdos: {
+        top: '30%',
         fontSize: 15,
         fontWeight: 'bold',
         color: '#625d5b'
     },
     containerunorama: {
-        marginTop: '10%',
+        marginTop: '0%',
         flexDirection: 'row',
         //container para los textInput
     },
     containerdos: {
-        marginTop: '10%',
+        marginTop: '25%',
     },
     TextInput: {
-        
+        top: '70%',
         flexDirection: 'row',
         width: 50,
         height: 50,
@@ -85,6 +130,19 @@ const styles = StyleSheet.create({
         borderColor: 'purple',
         margin: 5,
         paddingLeft: 20,
+        borderRadius: 10
+        
+    },
+    TextInput2: {
+        top: '155%',
+        flexDirection: 'row',
+        width: 50,
+        height: 50,
+        borderWidth: 1,
+        borderColor: 'purple',
+        margin: 5,
+        paddingLeft: 20,
+        borderRadius: 10
     },
     ButtonContainer: {
         elevation: 8,
@@ -99,4 +157,17 @@ const styles = StyleSheet.create({
         fontWeight: "bold",
         alignSelf: "center",
       },
+    botonCont: {
+        top: '100%',
+        backgroundColor: '#5B298A',
+        width: '80%',
+        alignItems: 'center',
+        borderRadius: 20
+    },
+    textoBoton: {
+        fontSize: 20,
+        color: 'white',
+        textAlign: 'center',
+        padding: 10,
+    },
 })
--- src/screens/Importarcuenta.tsx
+++ src/screens/Importarcuenta.tsx
@@ -10,26 +10,30 @@ import { generateMnemonic, mnemonicToSeed, createAccount, getBalance, getToken,s
 const ImportarCuenta = () => {
     return (
         <KeyboardAwareScrollView
-      resetScrollToCoords={{ x: 0, y: 0 }}
-      contentContainerStyle={styles.body}
-      scrollEnabled={false} >
+            resetScrollToCoords={{ x: 0, y: 0 }}
+            contentContainerStyle={styles.body}
+            scrollEnabled={false} >
             <View style={styles.containeruno}>
-            <Image source={require('./img/mountain.png')} style={styles.logo} />
-            <Text style={styles.text1} numberOfLines={2}>IMPORTAR CUENTA</Text>
+                <Image source={require('./img/logocolor.png')} style={styles.logo} />
             </View>
-            <View style={styles.containerunorama}>
-                {/*<TextInput  multiline={true} style={styles.TextInput} >{words}</TextInput>*/}
-                <TextInput style={styles.TextInput} keyboardType="default"></TextInput>
+            <View style={styles.containerBlanco}>
+            </View>
+            <View style={styles.containerBlanco2}>
+                <Text style={styles.textuno} numberOfLines={2}>IMPORTAR CUENTA</Text>
+                <TextInput 
+                    style={styles.TextInput}>
+                    <Text style={styles.labeluno} ></Text>
+                </TextInput>
             </View>
             <View style={styles.label}>
                 <Text style={styles.doce}>Ingrese sus 12 (doce) palabras de respaldo.</Text>
             </View>
             <View style={styles.containerdos}>
-            <TouchableOpacity
-                style={styles.botonCont}
-                onPress={() => Alert.alert('Acepto')}>
-                <Text style={styles.textoBoton}>ACEPTAR</Text>
-            </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.botonCont}
+                    onPress={() => Alert.alert('Acepto')}>
+                    <Text style={styles.textoBoton}>ACEPTAR</Text>
+                </TouchableOpacity>
             </View>
         </KeyboardAwareScrollView>
     )
@@ -42,22 +46,41 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '100%',
         flex: 1,
-        paddingLeft: '5%',
-        paddingRight: '5%',
         paddingTop: '8%'
     },
     containeruno: {
         alignItems: 'center',
-
+    },
+    containerBlanco: {
+        width: '100%',
+        marginTop: 10,
+        alignItems: 'center',
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+        borderWidth: 5,
+        borderColor: '#b5b2b6',
+        borderStyle: 'solid',
+        paddingTop: '35%',
+    },
+    containerBlanco2: {
+        width: '100%',
+        color: 'red',
+        marginTop: '-36%',
+        borderTopLeftRadius:28,
+        borderTopRightRadius: 28,
+        alignItems: 'center',
+        backgroundColor: 'white',
+        paddingLeft: '10%',
+        paddingRight: '10%',
+        paddingTop: '10%',
     },
     logo: {
-        marginTop: '25%',
+        marginTop: '0%',
         width: 150,
         height: 150,
         resizeMode: 'contain',
     },
-    text1: {
-        fontFamily: 'Roboto',
+    textuno: {
         fontSize: 15,
         fontWeight: 'bold',
         color: '#625d5b'
@@ -73,29 +96,32 @@ const styles = StyleSheet.create({
     },
     containerdos: {
         marginTop: '10%',
+        marginLeft: '2%',
+        marginRight: '2%',
         flexDirection: 'column',
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
     },
     TextInput: {
         fontSize: 20,
         margin: 10,
         width: 300,
         height: 200,
-        borderWidth: 1,
+        borderWidth: 2,
         borderColor: 'purple',
         borderRadius: 20,
         padding: 50,
         paddingLeft: 10,
         paddingRight: 10,
+        
     },
     botonCont: {
-        elevation: 8,
+        marginBottom: 20,
         top: 20,
         backgroundColor: '#5B298A',
         width: '80%',
         alignItems: 'center',
-        borderRadius: 20
+        borderRadius: 8
     },
     textoBoton: {
         fontSize: 20,
@@ -107,12 +133,25 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         marginBottom: 20,
         justifyContent: 'center',
+
     },
     checkbox: {
         alignSelf: "center",
     },
     doce: {
         margin: 8,
-        color: '#9c9897'
+        padding: 10,
+        fontWeight: 'bold',
+        fontSize: 12,
+        justifyContent: 'center',
+        textAlign: 'justify',
+        color: '#9c9897',
+    },
+        
+    labeluno: {
+        margin: 8,
+        fontWeight: 'bold',
+        fontSize: 17,
+        
     },
 })
