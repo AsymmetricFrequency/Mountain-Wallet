b3a854126441e3b6b4bc844d4ea7a536876e59f5
DarlinCondC <darlinapraez@condorcoin.co>
2021-12-10 10:52:51 -0500

    ventana splash, imagenes

--- src/screens/Splashc.tsx
+++ src/screens/Splashc.tsx
@@ -4,140 +4,73 @@ import { generateMnemonic, mnemonicToSeed, createAccount, getBalance, getToken,s
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 import { LinearGradient } from 'expo-linear-gradient';
 import { Pressable, Hoverable, ScrollView } from "react-native-web-hover";
-// import { TextInput } from 'react-native-element-textinput';
-
+import * as Animatable from 'react-native-animatable';
+import { useNavigation } from '@react-navigation/native';
 
 
 const Splashc = () => {
 
+    const navigation = useNavigation();
+
+    setTimeout(() => {
+        navigation.navigate("Home" as any)
+    }, 2500);
+
     return (
         <View style={styles.body}>
-            <ImageBackground source={require('./img/fondo.png')} style={styles.fondo} >
-                <View style={styles.containeruno}>
-                    <Image style={styles.logo} source={require('./img/enviar.png')}  />
-                    {/*Boton Depositar */}
-                    <View style={styles.cuadroD}>
-                        <TouchableOpacity style={styles.btnD}  activeOpacity={0.9}>
-                            <Text style={styles.textbtnD}>DEPOSITAR</Text> 
-                        </TouchableOpacity>
-                    </View>
-                    {/* Email */}
-                    <View style={styles.cuadro}>
-                        <View style={styles.tablamail} >
-                            <View style={styles.cuadromail}>
-                                <TextInput style={styles.inputmail} placeholder="DIRECCIÓN: XXXXXXXXXX@CNDR.com " />
-                            </View>
-                            <View style={styles.smcry}>
-                                <View style={styles.saldocry}>
-                                    <Text style={styles.stxtcry}>1000</Text>
-                                </View>
-                                <View style={styles.monedacry}>
-                                    <Text style={styles.mtxtcry}>CNDR</Text>
-                                </View>
-                            </View>                      
-                        </View>
-                    </View>
-                </View>             
-            </ImageBackground>   
+
+          <View style={styles.containeruno}>
+            {/* <Image style={styles.logorigin} source={require('./img/logocolor.png')}  /> */}
+            <Animatable.View 
+                    animation="fadeInDownBig"
+                    duration={2000}>
+                    <Image style={styles.logo} source={require('./img/mm1.png')}  />
+            </Animatable.View>
+            <Animatable.View
+                    animation="fadeInUp"
+                    duration={2000}>
+                    <Image style={styles.logo2} source={require('./img/mm2.png')}  />
+                    <Image style={styles.letras} source={require('./img/mm3.png')}/>
+            </Animatable.View>
+          </View>
         </View>
     )
 }
 
+export default Splashc
 
 const styles = StyleSheet.create({
-    body: {
-        width: '100%',
-        height: '100%',
-        flex: 1,
-    },
-    containeruno:{
-        paddingTop: '8%',
-        paddingLeft: '5%',
-        paddingRight: '4%',
-        alignItems:'center',
-    },
-    fondo:{
-        flex: 1,
-        resizeMode:'contain',
-    },
-    logo:{
-        width: 310,
-        height: 250,
-        top:'4%',
-        resizeMode: 'contain',
-    },
-    cuadroD:{
-        backgroundColor: 'white',
-        padding: 20,
-        borderRadius: 10,
-        marginTop: '5%',
-        width: '100%'
-    },
-    btnD:{
-        backgroundColor:'#5b298a',
-        alignItems:'center',
-        marginRight: '15%',
-        marginLeft: '15%',
-        paddingTop: '4%',
-        paddingBottom: '4%',
-        borderRadius: 20,
-    },
-    textbtnD:{
-        color:'white',
-        fontWeight: 'bold',
-        fontSize:RFPercentage(2),
-    },
-    cuadro:{
-        backgroundColor:'white',
-        width: '100%',
-        height: '100%',
-        marginTop: '3%',
-        borderTopRightRadius: 20,
-        borderTopLeftRadius: 20,
-        padding: '2%',
-    },
-    tablamail:{
-        marginTop:'4%',
-        borderWidth: 0.8,
-        borderColor: '#e0e0e0',
-        borderRadius:10,
-        height: '5.8%',
-        flexDirection:'row',
-        paddingLeft:'2.5%',
-        paddingRight:'3.5%',
-        paddingTop:'0%'
-    },
-    cuadromail:{
-        width:'80%',
-        justifyContent: 'center',
-        paddingLeft: '2%'
-    },
-    inputmail:{
-        fontWeight: 'bold',
-        fontSize:RFPercentage(1.8),
-        color: '#5a5959',
-    },
-    smcry:{
-        width:'20%',
-        alignItems: 'flex-end',
-        justifyContent: 'center',
-    },
-    saldocry:{
-        justifyContent: 'center',
-        alignItems: 'flex-end',
-    },
-    stxtcry:{
-        fontFamily: 'Roboto',
-        fontSize:RFPercentage(3),
-        color: '#8d8c8c',
-    },
-    monedacry:{
-        justifyContent: 'center',
-    },
-    mtxtcry:{
-        fontFamily: 'Roboto',
-        fontSize:RFPercentage(1.8),
-        color: '#8d8c8c',
-    },
+  body: {
+      width: '100%',
+      height: '100%',
+      flex: 1,
+      justifyContent: 'center',
+  },
+  containeruno:{
+      paddingLeft: '5%',
+      paddingRight: '4%',
+      alignItems:'center',
+  },
+  logorigin:{
+      width: 250,
+      height: 250,
+      resizeMode: 'contain',
+  },
+  logo:{
+    width: 250,
+    height: 250,
+    resizeMode: 'contain',
+    top: '100%',
+  },
+  logo2:{
+      width: 250,
+      height: 250,
+      resizeMode: 'contain',
+  },
+  letras: {
+      width: 250,
+      height: 250,
+      resizeMode: 'contain',
+      top: '-50%',
+  },
 })
-export default Splashc
