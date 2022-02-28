776c9d87dd7417c53f2365ebbad8c2e4ebc6ddf7
Faluraga <faluraga2021@gmail.com>
2022-02-28 16:58:04 -0500

    Se agrega logica funcional de cerrar sesion

--- src/screens/Ajustes.tsx
+++ src/screens/Ajustes.tsx
@@ -9,18 +9,45 @@ import {
   TouchableOpacity,
   Switch,
   SafeAreaView,
+  Alert,
+  Modal
 } from "react-native";
 import React, { useState } from "react";
 import { styles } from "../theme/appTheme";
 import Icon from "react-native-vector-icons/FontAwesome5";
 import Icons from "react-native-vector-icons/MaterialCommunityIcons";
 import { useTheme } from "react-native-paper";
-
+import { AsyncStorage } from 'react-native';
+import * as Animatable from "react-native-animatable";
+import LottieView from "lottie-react-native";
 import { readPublicKey, readKey, readUser } from "../../api";
+import { LotieCerrarSesion} from "./component/lottie";
 
 const altura = Platform.OS === "ios" ? 22 : 25;
 
 const Ajustes = ({ navigation, route }: { navigation: any; route: any }) => {
+
+  //Modales
+  const [anmt, setanmt] = useState("");
+  const [vacioModal, setVacioModal] = useState(false);
+
+  //Funcion para cerrar sesion y eliminar todos los datos del async storage 
+  const deleteAllData = () => {
+    
+    setVacioModal(true);
+    setanmt("zoomIn");
+    setTimeout(() => {
+      setanmt("zoomOut");
+      setTimeout(() => {
+        setVacioModal(false);
+      }, 100);
+      navigation.navigate("Home")
+    }, 3000);
+    setTimeout(()=>{
+      AsyncStorage.clear(() => {});
+    },3500)
+  };
+
   const [publicKey, setPublicKey] = useState("");
   const [secretKey, setSecretKey] = useState("");
 
@@ -59,8 +86,39 @@ const Ajustes = ({ navigation, route }: { navigation: any; route: any }) => {
   var strLastThree = str.substring(str.length - 5, str.length);
   var concatenado = `${strFirstThree}...${strLastThree}`;
 
+ 
+
   return (
     <SafeAreaView style={styles.body}>
+
+      <Modal
+        visible={vacioModal}
+        transparent
+        onRequestClose={() => setVacioModal(false)}
+        hardwareAccelerated
+      >
+        <View style={styles.cajafull}>
+                        <Animatable.View animation={anmt} duration= {600}>
+                                <View style={styles.ventanafull}>
+                                    <View style={styles.icontextfull}>
+                                        <View style={styles.contenedorlottiefull}>
+                                            <LotieCerrarSesion/>
+                                            <View style={{position:"absolute"}}>
+                                              <Icons name="logout" size={25} color="white" />
+                                            </View>
+                                            
+                                        </View>
+                                    </View>
+                                </View>
+                        </Animatable.View>
+                    </View>
+      </Modal>
+
+
+
+
+
+
       <View style={styles.completo}>
         <View style={styles.titlecc}>
           <Text style={styles.titletx}>Configuración</Text>
@@ -126,7 +184,7 @@ const Ajustes = ({ navigation, route }: { navigation: any; route: any }) => {
             </View>
           </TouchableOpacity>
 
-          <TouchableOpacity style={styles.cajaaj} activeOpacity={0.5}>
+          <TouchableOpacity style={styles.cajaaj} activeOpacity={0.5} onPress={() => deleteAllData()} >
             <View style={styles.imgaj}>
               <View style={styles.btnaj}>
                 <Icons name="logout" size={altura} color="#440577" />
--- src/screens/Exclave.tsx
+++ src/screens/Exclave.tsx
@@ -7,16 +7,21 @@ import {
   Platform,
   Image,
   Appearance,
+  Clipboard,
+  ToastAndroid,
+  Alert,
 } from "react-native";
 import React, { useState } from "react";
 import { styles } from "../theme/appTheme";
 import Icon from "react-native-vector-icons/FontAwesome5";
 import { useTheme } from "react-native-paper";
+import LottieView from "lottie-react-native";
 
 const altura = Platform.OS === "ios" ? 22 : 25;
 
 const Exclave = ({ navigation, route }: { navigation: any; route: any }) => {
   const secretKey = route.params?.llave_privada;
+  const [animacion, setAnimacion] = useState(false);
 
   //Detecta el modo del sistema
   const [theme, setTheme] = useState(Appearance.getColorScheme());
@@ -30,6 +35,40 @@ const Exclave = ({ navigation, route }: { navigation: any; route: any }) => {
   const mostrar = () => {
     setCambio(true);
   };
+
+  const CopyToClipboard = async () => {
+    Clipboard.setString(secretKey);
+    setAnimacion(true);
+    if (Platform.OS === "android") {
+      ToastAndroid.show("Clave privada copiada", ToastAndroid.SHORT);
+    } else {
+      Alert.alert("Clave privada copiada");
+    }
+  };
+  //Animacion copiado
+  const animation = React.useRef(null);
+  const amj = (number = 0) => {
+    if (Platform.OS === "android") {
+      number = 2000;
+    } else if (Platform.OS === "ios") {
+      number = 3000;
+    }
+    return number;
+  };
+  React.useEffect(() => {
+    if (animacion === true) {
+      animation.current.play(0, 50);
+      setTimeout(() => {
+        setAnimacion(false);
+      }, amj());
+    } else if (animacion === false) {
+      if (Platform.OS === "android") {
+        animation.current.play(18, 18);
+      } else if (Platform.OS === "ios") {
+        animation.current.play(18, 50);
+      }
+    }
+  }, [animacion]);
 
   return (
     <SafeAreaView style={styles.body}>
@@ -67,6 +106,26 @@ const Exclave = ({ navigation, route }: { navigation: any; route: any }) => {
             <Text style={styles.txtDonex}>Mostrar clave privada</Text>
           </TouchableOpacity>
         </View>
+        <TouchableOpacity onPress={() => CopyToClipboard()} activeOpacity={0.5}>
+          <View style={styles.tablaqr}>
+            <View style={styles.cbtncop}>
+              <LottieView
+                ref={animation}
+                style={styles.lottiecopy}
+                source={require("../screens/Lottie/copy.json")}
+                autoPlay={false}
+                loop={false}
+              />
+            </View>
+            <View style={styles.cuadroqr}>
+              <Text numberOfLines={3} style={styles.txtqr}>
+                Copiar clave privada
+              </Text>
+
+              {/* <TextInput style={styles.inputqr} value={pKey} editable={false}/> */}
+            </View>
+          </View>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
